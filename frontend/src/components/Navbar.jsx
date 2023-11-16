90d69dc32e97bd96e4d038fd1a395316ecdfec5b
AQIB-NAWAB <aqibnawab1100@gmail.com>
2023-11-16 12:41:04 +0500

    Added navbar , home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
-
+import { useEffect, useState } from "react";
+import "./components.css"
+import {Link} from "react-router-dom"
 const Navbar = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const isScrolling = scrollY > 170; // Adjust the threshold as needed
+      setScrolling(isScrolling);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    <div>Navbar</div>
+    <>
+ <nav
+        className={`fixed flex flex-row justify-between items-center py-3 px-10 text-white ${
+          scrolling ? "bg-white text-black" : "bg-transparent"
+        }`}
+      >
+  <div className="logo ">
+    HPVS
+  </div>
+  <div className="links ">
+    <ul className='flex flex-row gap-8  '>
+      <li>
+      <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/services">Services</Link>
+      </li>
+      <li>
+        <Link to="/shipping">Shipping </Link>
+      </li>
+      <li>
+        <Link to="/storage">Storage</Link>
+      </li>
+      <li>
+        <Link to="/track">Track</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+      
+      
+      
+    </ul>
+  </div>
+  <button className="trackNowBtn shadow-xl">
+    Track Now
+  </button>
+</nav>
+
+    </>
   )
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Hero from '../../components/Hero'
+import "./Home.css"
+import { MdLocalShipping } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div className='text-3xl'>Home</div>
+    <div>
+     <Hero/>
+      {/* Tracking components */}
+      <div className="home_page_tracker py-12 flex flex-row justify-center items-center text-white">
+        <div className="content">
+          <h2 className='text-xl '>Track Shipping </h2>
+          <div className="custom_input flex   shadow-sm">
+          <button >
+
+          <MdLocalShipping className='text-xl'  />
+          </button>
+          <input type="text" placeholder='SGUxxxxxxxx'/>
+          <button>Track</button>
+          </div>
+          <div className="message">
+          Stay updated on your shipment!
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
